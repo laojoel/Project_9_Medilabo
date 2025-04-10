ca3d995ede95f5474d4115134efa22953e27a562
Joel LAO <joel.lao@axa.fr>
2025-04-10 14:42:37 +0200

    init dockerCompose and update cloudGateway wordings

---
+++ gateway-application/src/test/java/com/medilabo/gatewayapplication/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.gatewayapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
