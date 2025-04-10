9263ad10799e26af020a94b14cac46d4205a83dc
Joel LAO <joel.lao@axa.fr>
2025-04-10 15:13:40 +0200

    setup dockerFiles & creqte data.sql to fill test entries

---
+++ patient-application/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO patient (firstName, lastName, dateOfBirth, gender, address, phoneNumber) VALUES
+('Jack',  'void',   '1962-08-21', 'M', '1 saturn street', '1111222233'),
+('Tina',  'Near',   '1988-06-14', 'F', '2 frenzy street', '4444555566'),
+('Fredy', 'Danger', '1995-02-23', 'M', '3 omega road',    '7777888899'),
+('Diana', 'Early',  '2001-09-11', 'F', '4 dragon road',   '8888999900');
