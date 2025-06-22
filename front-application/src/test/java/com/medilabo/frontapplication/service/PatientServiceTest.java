bc26c94cabc543ea683185711a4286e37f2c5fac
Joel LAO <joel.lao@axa.fr>
2025-06-22 12:21:06 +0200

    Services and Notes Test setup

--- front-application/src/test/java/com/medilabo/frontapplication/service/PatientServiceTest.java
+++ front-application/src/test/java/com/medilabo/frontapplication/service/PatientServiceTest.java
@@ -7,8 +7,10 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
@@ -21,6 +23,7 @@ import com.medilabo.frontapplication.model.Patient;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.times;
@@ -57,7 +60,7 @@ public class PatientServiceTest {
         ResponseEntity<List<Patient>> responseEntity = new ResponseEntity<>(patients, HttpStatus.OK);
 
         when(authRestTemplate.exchange(
-                eq(PATIENT_URI + "/all"),
+                eq(PATIENT_URI),
                 eq(HttpMethod.GET),
                 isNull(),
                 ArgumentMatchers.<ParameterizedTypeReference<List<Patient>>>any()
@@ -70,7 +73,7 @@ public class PatientServiceTest {
         assertNotNull(result);
         assertEquals(2, result.size());
         verify(authRestTemplate, times(1)).exchange(
-                eq(PATIENT_URI + "/all"),
+                eq(PATIENT_URI),
                 eq(HttpMethod.GET),
                 isNull(),
                 ArgumentMatchers.<ParameterizedTypeReference<List<Patient>>>any()
@@ -82,66 +85,62 @@ public class PatientServiceTest {
     @Test
     void getPatientId_Test() {
         // Arrange
-        when(routes.getAllPatientUri()).thenReturn(PATIENT_URI);
+        long patientId = 8L;
+        when(routes.getPatientUri()).thenReturn(PATIENT_URI);
 
-        Patient patient0 = new Patient();
-        patient0.setFirstName("Frank");
-        Patient patient1 = new Patient();
-        patient1.setFirstName("Sophie");
-        List<Patient> patients =  Arrays.asList(patient0, patient1);
-        ResponseEntity<List<Patient>> responseEntity = new ResponseEntity<>(patients, HttpStatus.OK);
+        Patient patient = new Patient();
+        patient.setFirstName("Frank");
+        ResponseEntity<Patient> responseEntity = new ResponseEntity<>(patient, HttpStatus.OK);
 
         when(authRestTemplate.exchange(
-                eq(PATIENT_URI + "/all"),
+                eq(PATIENT_URI+"/"+patientId),
                 eq(HttpMethod.GET),
                 isNull(),
-                ArgumentMatchers.<ParameterizedTypeReference<List<Patient>>>any()
+                ArgumentMatchers.<ParameterizedTypeReference<Patient>>any()
         )).thenReturn(responseEntity);
 
         // Act
-        List<Patient> result = patientService.getAllPatients();
+        Patient result = patientService.getPatientId(patientId);
 
         // Assert
         assertNotNull(result);
-        assertEquals(2, result.size());
         verify(authRestTemplate, times(1)).exchange(
-                eq(PATIENT_URI + "/all"),
+                eq(PATIENT_URI+"/"+patientId),
                 eq(HttpMethod.GET),
                 isNull(),
-                ArgumentMatchers.<ParameterizedTypeReference<List<Patient>>>any()
+                ArgumentMatchers.<ParameterizedTypeReference<Patient>>any()
         );
-        assertEquals("Frank", result.getFirst().getFirstName());
-        assertEquals("Sophie", result.get(1).getFirstName());
+        assertEquals("Frank", result.getFirstName());
     }
 
     @Test
-    void create_shouldReturnCreatedNote() {
+    void create_Test() {
         // Arrange
-        when(routes.getNoteCreationUri()).thenReturn(NOTE_URI);
+        when(routes.getNoteCreationUri()).thenReturn(PATIENT_URI);
 
-        Note note = new Note();
-        note.setContent("test content");
-        ResponseEntity<Note> responseEntity = new ResponseEntity<>(note, HttpStatus.CREATED);
+        Patient newPatient = new Patient();
+        newPatient.setFirstName("Frank");
 
+        ResponseEntity<Patient> responseEntity = new ResponseEntity<>(newPatient, HttpStatus.OK);
         when(authRestTemplate.exchange(
-                eq(NOTE_URI),
+                eq(PATIENT_URI),
                 eq(HttpMethod.POST),
-                ArgumentMatchers.<HttpEntity<Note>>any(),
-                ArgumentMatchers.<ParameterizedTypeReference<Note>>any()
+                any(),
+                ArgumentMatchers.<ParameterizedTypeReference<Patient>>any()
         )).thenReturn(responseEntity);
 
         // Act
-        Note result = noteService.create(note);
+        Patient result = patientService.create(newPatient);
 
         // Assert
         assertNotNull(result);
         verify(authRestTemplate, times(1)).exchange(
-                eq(NOTE_URI),
+                eq(PATIENT_URI),
                 eq(HttpMethod.POST),
-                ArgumentMatchers.<HttpEntity<Note>>any(),
-                ArgumentMatchers.<ParameterizedTypeReference<Note>>any()
+                ArgumentMatchers.<HttpEntity<Patient>>any(),
+                ArgumentMatchers.<ParameterizedTypeReference<Patient>>any()
         );
-        assertEquals("test content", result.getContent());
+        assertEquals("Frank", result.getFirstName());
     }
 
 
